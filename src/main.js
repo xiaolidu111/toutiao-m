228cac4cde06030c7dd07aabc0eca7f9bc7417de
itheima <itheima@itcast.cn>
2022-04-04 23:43:16 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 // 加载全局样式
 import './styles/index.less'
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载vant 全局样式
+import 'vant/lib/index.css'
+// 注册使用vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
